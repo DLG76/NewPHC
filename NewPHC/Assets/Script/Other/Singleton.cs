e88b8e5a9294e794681959826929ffdd438cd88c
teaVSmelon <pnwathnw@gmail.com>
2025-02-19 00:40:40 +0700

    stage system

---
+++ NewPHC/Assets/Script/Other/Singleton.cs
@@ -0,0 +1,67 @@
+using UnityEngine;
+
+// https://gist.github.com/mstevenson/4325117
+public class Singleton<T> : MonoBehaviour
+where T : Component
+{
+    private static T _instance;
+    public static T Instance
+    {
+        get
+        {
+            if (_instance == null)
+            {
+                var objs = FindObjectsOfType(typeof(T)) as T[];
+                if (objs.Length > 0)
+                    _instance = objs[0];
+                if (objs.Length > 1)
+                {
+                    Debug.LogError("There is more than one " + typeof(T).Name + " in the scene.");
+                }
+                if (_instance == null)
+                {
+                    GameObject obj = new GameObject();
+                    obj.hideFlags = HideFlags.HideAndDontSave;
+                    _instance = obj.AddComponent<T>();
+                }
+            }
+            return _instance;
+        }
+    }
+}
+
+
+public class SingletonPersistent<T> : MonoBehaviour where T : Component
+{
+    private static T instance;
+    public static T Instance
+    {
+        get
+        {
+            if (instance == null)
+            {
+                instance = FindObjectOfType<T>();
+                if (instance == null)
+                {
+                    GameObject singletonObject = new GameObject(typeof(T).Name);
+                    instance = singletonObject.AddComponent<T>();
+                    DontDestroyOnLoad(singletonObject);
+                }
+            }
+            return instance;
+        }
+    }
+
+    public virtual void Awake()
+    {
+        if (instance == null)
+        {
+            instance = this as T;
+            DontDestroyOnLoad(gameObject);
+        }
+        else if (instance != this)
+        {
+            Destroy(gameObject);
+        }
+    }
+}
